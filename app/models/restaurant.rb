4c362ff2d8e2b512ca32c4d71a0d889006c62e7a
venkata chitturi <venkatchitturi1@gmail.com>
2017-02-17 15:17:46 -0800

    02/17/17 first Commit

---
+++ app/models/restaurant.rb
@@ -0,0 +1,3 @@
+class Restaurant < ActiveRecord::Base
+  attr_accessible :address, :emimail, :name, :phone
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+YelpDemo::Application.config.secret_token = 'f53d5cec8fc07a60774df05ca0936bb39c7c5f19de0230c11c8cd2b4b7309e203cd47225ec22da24082ba80398d66c6e38afb27fc12818f813561f8e26276d98'
